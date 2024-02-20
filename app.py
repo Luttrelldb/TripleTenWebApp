989f2256a6939a668c728f04d368b71383665208
Daniel Luttrell <luttrelldb@gmail.com>
2024-02-20 18:35:11 -0500

    Fixed an issue with the wrong chart displaying and added some text

--- app.py
+++ app.py
@@ -3,19 +3,22 @@ import pandas as pd
 import plotly.express as px
 
 
-st.header('Tossing a Coin')
+st.header('Data Trends in Vehicle Data')
 
 # Read in Dataset
 df_vehicles = pd.read_csv("vehicles_us.csv")
 
 fig_price_dirst = px.histogram(df_vehicles['price'])
 
+
+st.write('The Price dirstribution of the vheicles is very right skwed.') 
+
 st.plotly_chart(fig_price_dirst)
+
+st.write('The most intresting trend is with the condition of the vehicles,  it seems that "good" contion is all that is required to get a good sale price for the veichles.') 
 
 fig_price_vs_condition = px.scatter(x=df_vehicles['condition'],y=df_vehicles['price'])
 
-st.plotly_chart(fig_price_dirst)
+st.plotly_chart(fig_price_vs_condition)
 
-st.header('Tossing a Coin')
 
-st.write('It is not a functional application yet. Under construction.') 
