0c2b6d49d1a8ed1d5c8dfb4ce1045521563f9b09
Daniel Luttrell <luttrelldb@gmail.com>
2024-02-20 18:25:40 -0500

    Updated requiremnts to include plotly and started adding charts to app

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+st.header('Tossing a Coin')
+
+# Read in Dataset
+df_vehicles = pd.read_csv("vehicles_us.csv")
+
+fig_price_dirst = px.histogram(df_vehicles['price'])
+
+st.plotly_chart(fig_price_dirst)
+
+fig_price_vs_condition = px.scatter(x=df_vehicles['condition'],y=df_vehicles['price'])
+
+st.plotly_chart(fig_price_dirst)
 
 st.header('Tossing a Coin')
 
